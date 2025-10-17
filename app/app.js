b631a2a6c008d0c9b2d30c1a56b95eaaeedb9419
s1ckret <ivan.kovaalenko@s1ckret.com>
2025-10-17 21:58:22 +0300

    feat: implement profile page
    Signed-off-by: s1ckret <ivan.kovaalenko@s1ckret.com>

--- app/app.js
+++ app/app.js
@@ -8,6 +8,7 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import passport from 'passport';
 import 'dotenv/config'
+import methodOverride from 'method-override';
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
@@ -58,7 +59,15 @@ app.set("layout extractScripts", true)
 app.use(expressLayouts);
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride(function(req, res){
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,9 +1,35 @@
 import express from 'express';
 const router = express.Router();
+import { User } from '../services/users-service.js';
 
 // GET Profile page
-router.get('/profile', (req, res) => {
-  res.render('profile', { title: 'Profile' });
+router.get('/profile', async (req, res) => {
+  res.render('profile', {
+    title: 'Profile',
+    user: req.user,
+  });
+});
+
+// PATCH Profile update
+router.patch('/profile', async (req, res) => {
+  const userId = req.user.id;
+  const { callsign, joined_at } = req.body;
+  try {
+    const updatedUser = await User.update(userId, { callsign, joined_at });
+    const success = 'Профіль успішно оновлено.';
+    res.render('profile', {
+      title: 'Profile',
+      user: updatedUser,
+      success,
+    });
+  } catch (error) {
+    error = 'Помилка оновлення профілю: ' + (error.message || error);
+    res.render('profile', {
+      title: 'Profile',
+      user: req.user,
+      error,
+    });
+  }
 });
 
 export default router;
